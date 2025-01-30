d0dfef8fc0f8a7a7e099d0b78c4d7ce156e49000
Pedro Henrique Cassemiro da Silva <phcassemiro@hotmail.com>
2025-01-30 10:11:34 -0300

    Adicionando autores aos livros

--- src/app.js
+++ src/app.js
@@ -16,12 +16,4 @@ const app = express();
 routes(app)
 
 
-// app.delete("/livros/:id",(req,res)=> {
-//     const index = buscaLivro(req.params.id);
-//     livros.splice(index,1);
-//     res.status(200).send("Livro removido com sucesso");
-
-// });
-
-
 export default app;
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js"
 
 class LivroController {
 
@@ -30,10 +31,14 @@ class LivroController {
     };
 
     static async cadastrarLivro(req,res){
+        const novoLivro = req.body;
+
         try {
 
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+            const livroCriado = await livro.create(livroCompleto)
+            res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
 
         } catch (error) {
             res.status(500).json({message: `${error.message} - falha ao cadastrar o livro`})
